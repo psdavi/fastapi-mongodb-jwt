4a5ec58bbe091f1d8697f438ae1a4cc6acf664d7
davi_veri <davi.pires@veri.com.br>
2022-08-31 11:52:20 -0300

    Continuidade do projeto: add services e core.security

--- backend/app/api/api_v1/handlers/user.py
+++ backend/app/api/api_v1/handlers/user.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from app.schemas.user_schema import UserAuth
+from app.services.user_service import UserService
+import pymongo
 
 user_router = APIRouter()
 
-@user_router.get('teste')
+
+@user_router.post('/create', summary = "Create new User")
+async def create_user(data: UserAuth):
+    try:
+        await UserService.create_user(data)
+    except pymongo.errors.DuplicateKeyError:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+        )
+        
+
+
+""" @user_router.get('teste')
 async def teste():
-    return {"message": "user router working"}
+    return {"message": "user router working"} """
