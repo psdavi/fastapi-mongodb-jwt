e5433ec6ea6ec8ef8881386a4b09139942a89e1b
davi_veri <davi.pires@veri.com.br>
2022-09-01 12:27:52 -0300

    comitando antes de fazer alguma cagada pq vou testar uma coisa, parei no minuto 51

--- backend/app/api/api_v1/handlers/user.py
+++ backend/app/api/api_v1/handlers/user.py
@@ -9,10 +9,11 @@ user_router = APIRouter()
 @user_router.post('/create', summary = "Create new User")
 async def create_user(data: UserAuth):
     try:
-        await UserService.create_user(data)
+        return await UserService.create_user(data)
     except pymongo.errors.DuplicateKeyError:
         raise HTTPException(
             status_code = status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exists"
         )
         
 
